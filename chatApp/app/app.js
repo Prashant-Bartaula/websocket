3a53dc4d1bfc859eb744f4daafac22eee848fe90
I-amPrashant <airmax50cent@gmail.com>
2025-05-05 17:04:19 +0545

    create a simple duplex communication betweeen server and client

--- chatApp/app/app.js
+++ chatApp/app/app.js
@@ -2,6 +2,7 @@ const socket=new WebSocket('ws://localhost:3000');
 
 function sendMessage(e){
     e.preventDefault();
+    console.log(' i ran')
     const input=document.querySelector('input');
     if(input.value){
         socket.send(input.value);
@@ -10,3 +11,10 @@ function sendMessage(e){
      input.focus();
 }
 document.querySelector('form').addEventListener('submit',sendMessage);
+
+socket.addEventListener('message',({data})=>{
+    const ul=document.querySelector('ul');
+    const li=document.createElement('li');
+    li.textContent=data;
+    ul.appendChild(li);
+})
--- chatApp/server/index.js
+++ chatApp/server/index.js
@@ -1,9 +1,11 @@
-import ws from 'ws';
+import {WebSocketServer} from 'ws';
 
-const server=new ws.Server({port:3000});
+const server=new WebSocketServer({port:'3000'});
 
 server.on('connection',socket=>{
     socket.on('message', message=>{
-        console.log(message);
+        const b= Buffer.from(message)
+        console.log(b.toString())
+        socket.send(`${message}`);
     })
 });
